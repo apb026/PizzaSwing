50e0ebb15a5433b86bbc791f13c5b92103d2457a
apb026 <apb.sgsits@gmail.com>
2021-07-20 18:48:49 +0530

    Initial Commit of PizzaSwing from apb

---
+++ src/pizzaswing/PizzaSwing.java
@@ -0,0 +1,13 @@
+package pizzaswing;
+
+/**
+ *
+ * @author HP
+ */
+public class PizzaSwing 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello from my swing application");
+    }    
+}
